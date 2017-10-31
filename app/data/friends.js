88b38138123fee51c12015f9691fc960dacb0803
lavanya128 <lavanya128@gmail.com>
2017-10-31 17:12:40 -0400

    working app

--- app/data/friends.js
+++ app/data/friends.js
@@ -17,7 +17,7 @@ var friendsArray = [
   },
 
   {
-  name: 'Jhanvi Rakesh',
+  name: 'Scissor head',
     photo: 'https://vignette3.wikia.nocookie.net/pixar/images/a/af/Dory_FD.jpg/revision/latest?cb=20170807222556',
     scores: [
       '4',
@@ -30,6 +30,23 @@ var friendsArray = [
       '4',
       '3',
       '4'
+    ]
+},
+
+{
+  name: 'Princess Poppy',
+    photo: 'https://static.kidspot.com.au/cm_assets/227179/5-poppy-trolls-3d-animation-movie-preview-20170523113118.jpg~q75,dx720y-u0r1g0,c--.jpg',
+    scores: [
+      '1',
+      '5',
+      '2',
+      '2',
+      '1',
+      '5',
+      '5',
+      '4',
+      '3',
+      '1'
     ]
 }
 ]
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,111 +1,43 @@
-var friendsArray = require("../data/friends.js");
+var friendList = require('../data/friends.js');
 
-module.exports = function(app) {
-  
-  app.get("/api/friends", function(req, res) {
-    console.log(friendsArray);
-    res.json(friendsArray);
+module.exports = function(app){
+  //a GET route that displays JSON of all possible friends
+  app.get('/api/friends', function(req,res){
+    res.json(friendList);
   });
 
-app.post("/api/friends", function(req, res) {
-    
-    var newFriend = req.body;
-    console.log(friendsArray);
-    console.log(newFriend);
+  app.post('/api/friends', function(req,res){
+    //grabs the new friend's scores to compare with friends in friendList array
+    var newFriendScores = req.body.scores;
     var scoresArray = [];
-    var tDiff = 10000;
-    var matchName = "";
-    var matchImage= "";
-    for(var i = 0; i < friendsArray.length-1; i++)
-    {
-    	var diff = 0;
-      
-		  console.log(friendsArray[i].name);
-    	for(var j = 0; j < newFriend.scores.length; j++)
-    	{
-    		diff += (Math.abs(parseInt(newFriend.scores[j] - friendsArray[i].scores[j])));
-        scoresArray.push(diff);
-        //console.log(diff);
-    	}
-    	console.log(diff);
+    var bestMatch = 0;
+
+    //runs through all current friends in list
+    for(var i=0; i<friendList.length; i++){
+      var scoresDiff = 0;
+      //run through scores to compare friends
+      for(var j=0; j<newFriendScores.length; j++){
+        scoresDiff += (Math.abs(parseInt(friendList[i].scores[j]) - parseInt(newFriendScores[j])));
+      }
+
+      scoresArray.push(scoresDiff);
       console.log(scoresArray);
-//new method for filter ES6.
-      //scoresArray.push(diff);
-      //diff = 0;
+    }
 
-      if (diff <= tDiff) {
-        console.log('Closest match found = ' + diff);
-        console.log('Friend name = ' + friendsArray[i].name);
-        console.log('Friend image = ' + friendsArray[i].photo);
-
-        totalDifference = diff;
-        matchName = friendsArray[i].name;
-        matchImage = friendsArray[i].photo;
+    //after all friends are compared, find best match
+    for(var i=0; i<scoresArray.length; i++){
+      if(scoresArray[i] <= scoresArray[bestMatch]){
+        bestMatch = i;
       }
     }
-    
-      friendsArray.push(newFriend);
-      //var bestMatch = friendsArray[scoresArray.indexOf(Math.min.apply(null, scoresArray))];
-      //console.log(bestMatch);
-      //res.send(bestMatch);
-      console.log(friendsArray);
-      res.json({status: 'OK', matchName: matchName, matchImage: matchImage});
-      
+
+    //return bestMatch data
+    var bff = friendList[bestMatch];
+    console.log(bff);
+    res.json(bff);
+
+    //pushes new submission into the friendsList array
+    friendList.push(req.body);
+    console.log(friendList);
   });
-
-
 };
-
-// var friendData = require('../data/friends.js');
-
-
-// module.exports = function (app) {
-
-//   app.get('/api/friends', function(req, res){
-//     res.json(friendData);
-//   })
-
-
-//   app.post('/api/friends', function(req, res){
-//     var newFriend = req.body;
-
-//     for(var i = 0; i < newFriend.scores.length; i++) {
-//       if(newFriend.scores[i] == "1 (Strongly Disagree)") {
-//         newFriend.scores[i] = 1;
-//       } else if(newFriend.scores[i] == "5 (Strongly Agree)") {
-//         newFriend.scores[i] = 5;
-//       } else {
-//         newFriend.scores[i] = parseInt(newFriend.scores[i]);
-//       }
-//     }
-
-//     var differencesArray = [];
-
-//     for(var i = 0; i < friendData.length; i++) {
-
-//       var comparedFriend = friendData[i];
-//       var totalDifference = 0;
-      
-//       for(var k = 0; k < comparedFriend.scores.length; k++) {
-//         var differenceOneScore = Math.abs(comparedFriend.scores[k] - newFriend.scores[k]);
-//         totalDifference += differenceOneScore;
-//       }
-
-//       differencesArray[i] = totalDifference;
-//     }
-
-//     var bestFriendNum = differencesArray[0];
-//     var bestFriendIndex = 0;
-
-//     for(var i = 1; i < differencesArray.length; i++) {
-//       if(differencesArray[i] < bestFriendNum) {
-//         bestFriendNum = differencesArray[i];
-//         bestFriendIndex = i;
-//       }
-//     }
-
-//     friendData.push(newFriend);
-//     console.log(friendData);
-//     res.json(friendData[bestFriendIndex]);
-//   })
-// }
